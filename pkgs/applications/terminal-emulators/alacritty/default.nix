9009266b40e7bcd7dfd69bc965343b14c05771d9
ren <546296+rencire@users.noreply.github.com>
2020-12-04 19:38:53 -0800

    wip

--- pkgs/applications/terminal-emulators/alacritty/default.nix
+++ pkgs/applications/terminal-emulators/alacritty/default.nix
@@ -52,16 +52,16 @@ let
 in
 rustPlatform.buildRustPackage rec {
   pname = "alacritty";
-  version = "0.6.0";
+  version = "05307f";
 
   src = fetchFromGitHub {
-    owner = "alacritty";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "vQdNwNiUvoJWRT1foPRadirI2zWjnzU3sGnIxeHKlj8=";
+    owner = "zenixls2";
+    repo = "alacritty";
+    rev = "05307f8782ae0c94da5ff7b4bab2d1a51a50a70a";
+    sha256 = "1acw7h92k75i9a8svaa8gxyjx57758vx5c237iq5q9i00s0a5ksq";
   };
 
-  cargoSha256 = "1PQSg6EmwVMZj2ALw6qsbtPMCtALVHx5TR05FjGD/QE=";
+  cargoSha256 = "0cb9mlpcydsanbcy5698w45jw8hss6fpz97g5f6aqcpbf9rg62r6";
 
   nativeBuildInputs = [
     cmake
